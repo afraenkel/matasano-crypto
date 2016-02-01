2c70a20790d96016e6de67e021a6fe4f5ee294e4
Aaron Fraenkel <aaron.fraenkel@gmail.com>
2016-02-01 00:03:08 -0800

    wrote padding oracle for 17

---
+++ challenge17.py
@@ -0,0 +1,34 @@
+import utils
+import random
+from Crypto.Cipher import AES
+
+CT_LIST = '\n'.split('''
+MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=
+MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=
+MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==
+MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==
+MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl
+MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==
+MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==
+MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=
+MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=
+MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93''')
+
+KEY = utils.rand_bytes(16)
+
+def enc_rand_ct():
+    iv = utils.rand_bytes(16)
+    pt = utils.pad(random.sample(CT_LIST, 1)[0])
+    ct = AES.new(KEY, AES.MODE_CBC, iv).encrypt(pt)
+    return ct, iv
+
+
+def padding_oracle(ct, iv):
+    pt = AES.new(KEY, AES.MODE_CBC, iv).decrypt(ct)
+    try:
+        utils.unpad(pt)
+        return True
+    except TypeError:
+        return False
+
+# To Do: implement the attack!
